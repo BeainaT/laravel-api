f1cbe6400438e88ec262cf5b36f0caa54f21c7ad
BeainaT <tania.df@hotmail.it>
2022-07-26 20:30:57 +0200

    axios call from vue+basis print home

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,4 +1,7 @@
 window.Vue = require('Vue');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; //csrf token from cookie to backend
+
 import App from './views/App.vue';
 import Vue from 'vue';
 
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    <h1>front office with vue js</h1>
+    <h1>front office with vue</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+
 export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        posts: []
+      };
+    },
+    created() {
+      axios.get('http://127.0.0.1:8000/api/posts')
+        .then(res => {
+          this.posts = res.data;
+        })
+        .catch(e => {
+          console.log(e);
+        })
+    }
 }
 </script>
 
